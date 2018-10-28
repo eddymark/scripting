69f14a90cc96c677e15cc06c9c60b3b69e7970bd
eddymark <eddyv5413@gmail.com>
2018-10-28 10:28:50 -0400

    practiced array

---
+++ array.sh
@@ -0,0 +1,23 @@
+ #creating an array 
+
+ #name[index] = value
+
+ students[0]=john
+ students[1]=barry
+ students[2]=albert
+ students[3]=jose
+ students[10]=juan
+
+ #making an array in one line 
+
+ fruit=(apple fun run)
+
+ #accesing the array
+
+ echo ${fruit[2]}
+
+ #prints all of the variables 
+
+ echo ${students[*]}
+
+ 
